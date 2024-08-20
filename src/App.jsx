1d2c66962db493b8e6b4121cf8bb37676dc68a6c
Talal Ait-Mouh <148981557+Talal-Aitmouh@users.noreply.github.com>
2024-08-20 01:47:41 +0100

    minores changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { motion } from 'framer-motion';
 import imageSrc from './assets/logo.png';
